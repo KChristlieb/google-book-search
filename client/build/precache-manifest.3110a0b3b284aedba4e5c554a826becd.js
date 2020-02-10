56ec52cdeba977f34b23e335073eafdb9ceb5b16
Kade Christlieb <kchristlieb@gmail.com>
2020-02-10 13:48:12 -0700

    created build and ready to delpoy.

---
+++ client/build/precache-manifest.3110a0b3b284aedba4e5c554a826becd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b87cf951255e9f3225e9",
+    "url": "/static/js/main.2b7c77da.chunk.js"
+  },
+  {
+    "revision": "f00c099dc81a89ad8d70",
+    "url": "/static/js/2.0cb22072.chunk.js"
+  },
+  {
+    "revision": "b87cf951255e9f3225e9",
+    "url": "/static/css/main.9542a0ff.chunk.css"
+  },
+  {
+    "revision": "5e0bf6d138de914bbf77c5efff1b7179",
+    "url": "/index.html"
+  }
+];
